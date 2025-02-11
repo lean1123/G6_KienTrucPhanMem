9a06dc33a03a8d787adad3b60ea3c80e98941a2b
HoangHuy-23 <hh.glorious@gmail.com>
2025-02-11 17:33:32 +0700

    docs: additional some file system

---
+++ shoes-store-backend/api-gateway/src/main/java/ktpm17ctt/g6/gateway/ApiGatewayApplication.java
@@ -0,0 +1,13 @@
+package ktpm17ctt.g6.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiGatewayApplication.class, args);
+    }
+
+}
---
+++ shoes-store-backend/identity-service/src/main/java/ktpm17ctt/g6/identity/IdentityServiceApplication.java
@@ -0,0 +1,13 @@
+package ktpm17ctt.g6.identity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IdentityServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IdentityServiceApplication.class, args);
+    }
+
+}
