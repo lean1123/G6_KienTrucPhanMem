1a5e49509ac6d35c11e3010ceabd946618a6b229
lean1123 <lethanhan20039@gmail.com>
2025-02-15 12:33:28 +0700

    initialize order-service

---
+++ shoes-store-backend/order-service/src/test/java/java/ktpm17ctt/g6/orderservice/OrderServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package java.ktpm17ctt.g6.orderservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
