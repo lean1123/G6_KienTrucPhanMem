531ed927c38dfa7490c04d4e27fefc8816e72210
Huy <phanngochuy.92.2017.2018@gmail.com>
2025-02-18 21:52:15 +0700

    feat(inventory): initialize inventory service

---
+++ shoes-store-backend/inventory-service/src/main/java/ktpm17ctt/g6/inventory/InventoryServiceApplication.java
@@ -0,0 +1,13 @@
+package ktpm17ctt.g6.inventory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InventoryServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InventoryServiceApplication.class, args);
+	}
+
+}
---
+++ shoes-store-backend/inventory-service/src/test/java/ktpm17ctt/g6/inventory/InventoryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package ktpm17ctt.g6.inventory;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InventoryServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
