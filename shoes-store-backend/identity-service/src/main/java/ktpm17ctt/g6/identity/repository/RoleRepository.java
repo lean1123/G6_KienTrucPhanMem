c9663edd9e070cf68b2c4159c0663399a94f7664
HoangHuy-23 <hh.glorious@gmail.com>
2025-03-03 22:41:59 +0700

    fix(main): roll back

--- shoes-store-backend/identity-service/src/main/java/ktpm17ctt/g6/identity/repository/RoleRepository.java
+++
@@ -1,9 +0,0 @@
-package ktpm17ctt.g6.identity.repository;
-
-import ktpm17ctt.g6.identity.entity.Role;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface RoleRepository extends JpaRepository<Role, String> {
-}
