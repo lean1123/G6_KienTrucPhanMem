a16ed9218ec5862b4852b66863edbca4435c6232
XuanKy <kyxuan701@gmail.com>
2025-04-17 11:13:25 +0700

    feat: add feedback service

---
+++ shoes-store-backend/feedback-service/src/test/java/ktpm17ctt/g6/feedback/FeedbackServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package ktpm17ctt.g6.feedback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FeedbackServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
